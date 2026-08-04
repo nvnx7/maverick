1a314eb5b818d9aa1164156af34de0e310201ff7
nvnx7 <thenvnsahu@gmail.com>
2026-08-04 16:22:04 +0530

    feat: add chain config

--- web/src/components/ui/provider.tsx
+++ web/src/components/ui/provider.tsx
@@ -1,15 +1,28 @@
-"use client"
+"use client";
 
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
-import {
-  ColorModeProvider,
-  type ColorModeProviderProps,
-} from "./color-mode"
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createConfig, http, WagmiProvider } from "wagmi";
+import { arcTestnet } from "wagmi/chains";
+import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
+
+export const wagmiConfig = createConfig({
+  chains: [arcTestnet],
+  transports: {
+    [arcTestnet.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
 
 export function Provider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
-    </ChakraProvider>
-  )
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider value={defaultSystem}>
+          <ColorModeProvider {...props} />
+        </ChakraProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
