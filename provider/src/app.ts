ca230fba85d55c73c797376245c71fcd8d2c584c
nvnx7 <thenvnsahu@gmail.com>
2026-08-07 13:27:33 +0530

    fix: add cors

--- provider/src/app.ts
+++ provider/src/app.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
+import { cors } from 'hono/cors'
 import { logger } from "hono/logger";
 import { routes } from "./routes";
 
-export const app = new Hono().use(logger()).route("/", routes);
+export const app = new Hono().use('*', cors()).use(logger()).route("/", routes);
