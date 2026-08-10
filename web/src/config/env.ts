e602c678e5445f30fdf7e28a1ede150c88390631
nvnx7 <thenvnsahu@gmail.com>
2026-08-10 22:29:15 +0530

    chore: add blockscout api

--- web/src/config/env.ts
+++ web/src/config/env.ts
@@ -7,4 +7,5 @@ export const rpcArcTestnet = process.env.NEXT_PUBLIC_RPC_ARC_TESTNET as string;
 export const apiProvider = process.env.NEXT_PUBLIC_API_PROVIDER as string;
 export const apiEvaluator = process.env.NEXT_PUBLIC_API_EVALUATOR as string;
 
+export const apiKeyBlockscout = process.env.NEXT_PUBLIC_API_KEY_BLOCKSCOUT as string;
 export const walletConnectProjectId = "maverick-app";
